0a7d87e1b5baa53b7b819a54a83b7d481242ca40
smartdev0612 <danieldev0612@gmail.com>
2022-11-08 19:57:24 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import PostCreate from './PostCreate';
 import PostList from './PostList';
@@ -6,7 +5,7 @@ import PostList from './PostList';
 function App() {
   return (
     <div className="container">
-      <h1>Create Post</h1>
+      <h1>Create Post!!!</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
